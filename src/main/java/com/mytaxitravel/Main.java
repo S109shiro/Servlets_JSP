d10c98976a812c28e15e4e478d8813e805aedf13
S109shiro <sebgonrod@gmail.com>
2025-11-26 23:39:24 -0500

    Cambio en la lectura de usuarios (id a numero_identidad)

--- src/main/java/com/mytaxitravel/Main.java
+++ src/main/java/com/mytaxitravel/Main.java
@@ -4,11 +4,11 @@ import java.sql.Date;
 
 public class Main {
     static void main(String[] args) {
-        Usuario persona = new Usuario("Juan", "Diaz", "Romero", (byte)14, 182282994, "sebrod@gmail.com", "Femenino", "url.com", "3003367605", Date.valueOf("2004-10-09"), 4.8f, "[]", "2004Sebas!");
+        Usuario persona = new Usuario("Juan", "Diaz", "Romero", (byte)14, 1016712994, "sebrod@gmail.com", "Femenino", "url.com", "3003367605", Date.valueOf("2004-10-09"), 4.8f, "[]", "2004Sebas!");
         Crud editar = new Crud();
 
-        //editar.modificarDatosUsuario("Fecha Nacimiento", "2004-10-09", 1);
-        //editar.leerDatosUsuario(1);
+        //editar.modificarDatosUsuario("numero identificacion", "1016712994", 1);
+        editar.leerDatosUsuario(persona.getNumeroIdentificacion());
 
     }
 
